38be5b09f276c56c7e80009c83ebf79f75368203
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-07-21 09:27:53 +0300

    Added GetUserByEmailDto + validator

---
+++ src/UserService.Application/DTOs/GetUserByEmailDto.cs
@@ -0,0 +1,6 @@
+﻿namespace UserService.Application.DTOs;
+
+public class GetUserByEmailDto
+{
+    public required string Email { get; set; }
+}
---
+++ src/UserService.Application/Validators/GetUserByEmailDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using UserService.Application.DTOs;
+
+namespace UserService.Application.Validators;
+
+public class GetUserByEmailDtoValidator : AbstractValidator<GetUserByEmailDto>
+{
+    public GetUserByEmailDtoValidator()
+    {
+        RuleFor(user => user.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email format.")
+            .MaximumLength(100).WithMessage("Email must not exceed 100 characters.");
+    }
+}
