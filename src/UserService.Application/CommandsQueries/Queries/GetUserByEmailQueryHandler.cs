78c122faad6a5fcc8dbacdcbe8b3b4d7760e6d0a
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-07-21 09:29:03 +0300

    Added GetUserByEmailQueryHandler

---
+++ src/UserService.Application/CommandsQueries/Queries/GetUserByEmailQueryHandler.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using FluentResults;
+using FluentValidation;
+using MediatR;
+using UserService.Application.DTOs;
+using UserService.Application.Interfaces;
+
+namespace UserService.Application.CommandsQueries.Queries;
+
+public record GetUserByEmailQuery(GetUserByEmailDto User) : IRequest<Result<GetUserDto>>;
+
+public class GetUserByEmailQueryHandler : IRequestHandler<GetUserByEmailQuery, Result<GetUserDto>>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+    private readonly IValidator<GetUserByEmailDto> _validator;
+
+    public GetUserByEmailQueryHandler(
+        IUserRepository userRepository,
+        IMapper mapper,
+        IValidator<GetUserByEmailDto> validator)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+        _validator = validator;
+    }
+
+    public async Task<Result<GetUserDto>> Handle(GetUserByEmailQuery request, CancellationToken cancellationToken)
+    {
+        var validationResult = await _validator.ValidateAsync(request.User, cancellationToken);
+
+        if (!validationResult.IsValid)
+        {
+            return Result.Fail(validationResult.Errors.ToString());
+        }
+
+        var user = await _userRepository.GetByEmailAsync(request.User.Email, cancellationToken);
+
+        if (user is null)
+        {
+            return Result.Fail("User not found");
+        }
+
+        var userDto = _mapper.Map<GetUserDto>(user);
+
+        return Result.Ok(userDto);
+    }
+}
