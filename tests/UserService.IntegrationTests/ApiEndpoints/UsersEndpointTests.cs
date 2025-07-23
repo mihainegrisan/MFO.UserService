191ff2d981825fcfd10ad62c14d64444427f128f
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-07-23 10:33:58 +0300

    Rearrange the folders and files for integration tests

---
+++ tests/UserService.IntegrationTests/ApiEndpoints/UsersEndpointTests.cs
@@ -0,0 +1,5 @@
+﻿namespace UserService.IntegrationTests.ApiEndpoints;
+
+public class UsersEndpointTests
+{
+}
---
+++ tests/UserService.IntegrationTests/Infrastructure/UserRepositoryTests.cs
@@ -0,0 +1,18 @@
+﻿namespace UserService.IntegrationTests.Infrastructure;
+
+public class UserRepositoryTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void CreateUser_ValidEmail_ReturnsUser()
+    {
+        // Arrange
+        // Act
+        // Assert
+        Assert.Pass();
+    }
+}
