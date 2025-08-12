2ad25fc4f1c061449095ad2ac13ad6d6ba67cc75
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-08-12 09:58:01 +0300

    Validate Queries

---
+++ src/UserService.Application/Validators/Queries/GetUserByIdQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using UserService.Application.CommandsQueries.Queries;
+
+namespace UserService.Application.Validators.Queries;
+
+public class GetUserByIdQueryValidator : AbstractValidator<GetUserByIdQuery>
+{
+    public GetUserByIdQueryValidator()
+    {
+        RuleFor(q => q.Id)
+            .NotEmpty().WithMessage("Id is required.");
+    }
+}
