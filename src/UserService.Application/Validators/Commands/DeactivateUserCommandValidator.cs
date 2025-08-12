ffd5ea1915ca87c6c1bf0cc4f9370843f1a7af41
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-08-12 09:58:24 +0300

    Validate Commands

---
+++ src/UserService.Application/Validators/Commands/DeactivateUserCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using UserService.Application.CommandsQueries.Commands;
+
+namespace UserService.Application.Validators.Commands;
+
+public class DeactivateUserCommandValidator : AbstractValidator<DeactivateUserCommand>
+{
+    public DeactivateUserCommandValidator()
+    {
+        RuleFor(q => q.Id)
+            .NotEmpty().WithMessage("Id is required.");
+    }
+}
---
+++ src/UserService.Application/Validators/Commands/DeleteUserCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using UserService.Application.CommandsQueries.Commands;
+
+namespace UserService.Application.Validators.Commands;
+
+public class DeleteUserCommandValidator : AbstractValidator<DeleteUserCommand>
+{
+    public DeleteUserCommandValidator()
+    {
+        RuleFor(q => q.Id)
+            .NotEmpty().WithMessage("Id is required.");
+    }
+}
