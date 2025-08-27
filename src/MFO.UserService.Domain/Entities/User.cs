172734851e2cf06c7c39a012f6982a8fc05284f4
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-08-27 20:17:37 +0300

    Extract properties into a common class AuditableEntity

--- src/MFO.UserService.Domain/Entities/User.cs
+++ src/MFO.UserService.Domain/Entities/User.cs
@@ -1,13 +1,19 @@
-﻿namespace MFO.UserService.Domain.Entities;
+﻿using MFO.UserService.Domain.Common;
 
-public class User
+namespace MFO.UserService.Domain.Entities;
+
+public class User : AuditableEntity
 {
     public Guid Id { get; set; }
+
     public string? FirstName { get; set; }
+
     public string? LastName { get; set; }
+
     public string? Email { get; set; }
+
     public string? PasswordHash { get; set; }
-    public DateTime CreatedAt { get; set; }
+
     public bool IsActive { get; set; } = true;
 
     public string FullName => $"{FirstName} {LastName}".Trim();
