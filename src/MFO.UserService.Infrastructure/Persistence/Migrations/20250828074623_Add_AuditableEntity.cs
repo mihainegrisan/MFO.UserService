83d27eb121f576e493f1953112f3fdf71e464e73
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-08-29 08:54:31 +0300

    Apply migrations and update database

---
+++ src/MFO.UserService.Infrastructure/Persistence/Migrations/20250828074623_Add_AuditableEntity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MFO.UserService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_AuditableEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Users",
+                newName: "CreatedDate");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastModifiedBy",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModifiedDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedBy",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedDate",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "Users",
+                newName: "CreatedAt");
+        }
+    }
+}
